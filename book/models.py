1fd8df5ad474b03c1eada0381dba39e228828401
Macan Mehri <macanmehri@gmail.com>
2024-03-17 21:20:47 +0330

    Created model of book

--- book/models.py
+++ book/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+
+    is_active = models.BooleanField(
+        default=False,
+        verbose_name='Is Active'
+    )
+
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created Date'
+    )
+
+    updated_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Updated Date'
+    )
+
+
+    class Meta:
+        abstract = True
+
+
+    def __str__(self) -> str:
+        raise NotImplementedError('You did not override the string method!')
+
+
+class Book(BaseModel):
+
+    title = models.CharField(
+        max_length=255,
+        blank=False,
+        verbose_name='Book Title'
+    )
+
+    writer = models.CharField(
+        max_length=255,
+        blank=False,
+        verbose_name='Book Writer'
+    )
+
+    rating = models.CharField(
+        max_length=255,
+        blank=False,
+        verbose_name='Book Rate'
+    )
+
+    published_year = models.IntegerField(verbos_name='Published Year')
+
+    number_of_editions = models.IntegerField(verbos_name='Number Of Editions')
